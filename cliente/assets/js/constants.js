cac4eda3bdcbcaaf8f59f8af4959a778c8e2b1ae
Ronald33 <Ronald_d33@hotmail.com>
2025-09-01 14:01:35 -0500

    Update constants.js

--- cliente/assets/js/constants.js
+++ cliente/assets/js/constants.js
@@ -6,8 +6,8 @@
         add_patata_rest_method: false, 
         state_initial: 
         {
-            'administrador': 'admin.usuario_lista', 
-            'operador': 'admin.movimiento_lista'
+            'administrador': 'admin.usuario_list', 
+            'operador': 'admin.movimiento_list'
         }
     });
 })();
